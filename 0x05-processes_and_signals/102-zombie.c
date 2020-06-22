5910ba00d24862cc756f2f1dd5a3c0eea9162690
David Orozco <dvdizcky@gmail.com>
2020-06-22 17:06:00 -0500

    Task 11: Betty style added

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -4,6 +4,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * infinite_while - Infinite loop with 1ms of delay
+ *
+ * Return: 0 but is an infinite loop without any kind
+ * of condition
+ **/
 int infinite_while(void)
 {
 	while (1)
@@ -13,6 +19,11 @@ int infinite_while(void)
 	return (0);
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ **/
 int main(void)
 {
 	pid_t pid;
@@ -22,14 +33,11 @@ int main(void)
 	{
 		pid = fork();
 		if (pid > 0)
-		{
 			printf("Zombie process created, PID: %d\n", pid);
-		}
-		if (pid == 0)
-		{
+		else if (pid == 0)
 			exit(0);
-		}
 	}
 	infinite_while();
-	return 0;
+
+	return (0);
 }
