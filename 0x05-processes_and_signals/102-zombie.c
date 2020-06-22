5d9d3b36be264ab3c15e0193a5e168179a12154f
David Orozco <dvdizcky@gmail.com>
2020-06-22 15:37:28 -0500

    Task 11 added

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+int main(void)
+{
+	pid_t pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
+		if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+			infinite_while();
+		}
+	}
+	return 0;
+}
