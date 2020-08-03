e1198f524bf0ff1c1f810bfe88f2e26d9d50baf3
Orozco David <dvdizcky@gmail.com>
2020-08-03 15:42:56 -0500

    Task 0: Group file fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Create a file named holberton in tmp with owner and group www-data
 file {'/tmp/holberton':
   owner   => 'www-data',
-  vgroup  => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
   mode    => '0744',
 }
