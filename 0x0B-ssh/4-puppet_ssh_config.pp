edd19b1973a795657abd8d715cb64d6e335171ad
Orozco David <dvdizcky@gmail.com>
2020-08-04 09:02:00 -0500

    Fixing dependencies

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,13 @@
 # Create ssh config in ~/.ssh/config on client
-file_line { '/config3':
-  path => '/home/ubuntu/.ssh/config',
+file {'/etc/ssh/ssh_config':
+  ensure => present,
+}
+-> file_line { '/config3':
+  ensure => present,
   line => 'Host *',
 }
-
-file_line { '/config3':
-  path => '/home/ubuntu/.ssh/config',
+-> file_line { '/config3':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
   line => '   IdentityFile ~/.ssh/holberton',
 }
