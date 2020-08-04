d40c2b0f5bed0714da383483d8a99ecfdc55a24c
Orozco David <dvdizcky@gmail.com>
2020-08-04 09:29:31 -0500

    Fixing path

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,11 +2,12 @@
 file {'/etc/ssh/ssh_config':
   ensure => present,
 }
--> file_line { '/config3':
+-> file_line { '/etc/ssh/ssh_config':
+  path => '/etc/ssh/ssh_config', 
   ensure => present,
   line => 'Host *',
 }
--> file_line { '/config3':
+-> file_line { '/etc/ssh/ssh_config':
   ensure => present,
   path => '/etc/ssh/ssh_config',
   line => '   IdentityFile ~/.ssh/holberton',
