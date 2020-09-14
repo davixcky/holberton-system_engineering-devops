65e8034072ea693db1226d62ca2c701333a762b4
Orozco David <dvdizcky@gmail.com>
2020-09-14 15:04:14 -0500

    Task 2 added

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" For a given employee, returns information about the TODO list progress"""
+import requests
+from sys import argv
+import json
+
+
+if __name__ == '__main__':
+    id_ = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(id_)
+
+    s = requests.Session()
+
+    url2 = 'https://jsonplaceholder.typicode.com/users/{}'.format(id_)
+    response = s.get(url2)
+    name = response.json()['name']
+
+    response = s.get(url)
+    body = response.json()
+
+    data = {id_: []}
+    for task in body:
+        data[id_].append({"task": "{}".format(task['title']),
+                          "completed": task['completed'],
+                          "username": "{}".format(name)})
+
+    with open(id_ + '.json', 'w') as outfile:
+        json.dump(data, outfile)
